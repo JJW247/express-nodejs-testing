4adbbc781928ba9b09332e89394a2635ebac3462
jjw247 <adr10won@gmail.com>
2022-07-11 00:20:24 +0900

    init testing

---
+++ routes/shop.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const rootDir = require('../util/path');
+const router = express.Router();
+router.get('/', (request, response, next) => {
+  response.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+module.exports = router;
